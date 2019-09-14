4389c8219a40ebed27399aa304d8525db5cb4d66
Touhid Mia <hmtm.cse@gmail.com>
2019-09-15 00:06:37 +0600

    Ascii doc

--- src/main/groovy/com/hmtmcse/gsrest/GSSpecification.groovy
+++ src/main/groovy/com/hmtmcse/gsrest/GSSpecification.groovy
@@ -2,7 +2,18 @@ package com.hmtmcse.gsrest
 
 import spock.lang.Specification
 
-class GSSpecification extends Specification {
+class GSSpecification<T> extends Specification {
+
+    public String apiURL = null
+
+    String getApiURL() {
+        return apiURL
+    }
+
+    T setApiURL(String apiURL) {
+        this.apiURL = apiURL
+        return T
+    }
 
 
 }
--- src/test/groovy/com/hmtmcse/gsrest/MyFirstSpecification.groovy
+++ src/test/groovy/com/hmtmcse/gsrest/MyFirstSpecification.groovy
@@ -1,6 +1,18 @@
 package com.hmtmcse.gsrest
 
 
-class MyFirstSpecification extends GSSpecification {
+class MyFirstSpecification extends GSSpecification<MyFirstSpecification> {
+
+
+
+    def "bismillah Test"(){
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a << [5, 3]
+        b << [1, 9]
+        c << [5, 9]
+    }
 
 }
