2e7fe69428e10580ff80506341e4eb23dbe40387
Touhid Mia <touhid@bitmascot.com>
2019-11-06 21:12:43 +0600

    Project

--- src/main/groovy/com/hmtmcse/gsrest/GsAPISpecification.groovy
+++ src/main/groovy/com/hmtmcse/gsrest/GsAPISpecification.groovy
@@ -1,10 +1,29 @@
 package com.hmtmcse.gsrest
 
+import com.hmtmcse.httputil.HttpResponse
 
 
 class GsAPISpecification extends RestAPISpecification {
 
 
+    private throwError(String defaultMessage, String errorMessage = null){
+        if (errorMessage){
 
+        }
+    }
+
+    public processResponse(HttpResponse response, String errorMessage = null){
+        if (!response){
+
+        }
+    }
+
+    public Map getJsonResponseContent(){
+        String response = this.apiResponse.content
+        if (response){
+            return stringToMap(response)
+        }
+        return null
+    }
 
 }
--- src/main/groovy/com/hmtmcse/gsrest/RestAPISpecification.groovy
+++ src/main/groovy/com/hmtmcse/gsrest/RestAPISpecification.groovy
@@ -2,6 +2,7 @@ package com.hmtmcse.gsrest
 
 import com.hmtmcse.httputil.HttpResponse
 import com.hmtmcse.httputil.HttpUtil
+import com.hmtmcse.jtfutil.parser.JsonReadWrite
 import spock.lang.Specification
 
 class RestAPISpecification extends Specification {
@@ -22,6 +23,24 @@ class RestAPISpecification extends Specification {
 
     HttpResponse getApiResponse(){
         return this.httpUtil.send()
+    }
+
+    String mapToString(Map data){
+        JsonReadWrite jsonReadWrite = new JsonReadWrite()
+        try{
+            return jsonReadWrite.objectAsJsonString(data)
+        }catch(Exception e){
+            return null
+        }
+    }
+
+    Map stringToMap(String data){
+        JsonReadWrite jsonReadWrite = new JsonReadWrite()
+        try{
+            return jsonReadWrite.readJsonStringToMap(data)
+        }catch(Exception e){
+            return null
+        }
     }
 
 }
