bc879f799c21cd488b9932824f49a4b2c32120dc
Touhid Mia <hmtm.cse@gmail.com>
2019-11-07 07:00:18 +0600

    Test System

--- src/main/groovy/com/hmtmcse/gsrest/GsAPISpecification.groovy
+++ src/main/groovy/com/hmtmcse/gsrest/GsAPISpecification.groovy
@@ -6,21 +6,21 @@ import com.hmtmcse.httputil.HttpResponse
 class GsAPISpecification extends RestAPISpecification {
 
 
-    private throwError(String defaultMessage, String errorMessage = null){
-        if (errorMessage){
+    private throwError(String defaultMessage, String errorMessage = null) {
+        if (errorMessage) {
 
         }
     }
 
-    public processResponse(HttpResponse response, String errorMessage = null){
-        if (!response){
+    public processResponse(HttpResponse response, String errorMessage = null) {
+        if (!response) {
 
         }
     }
 
-    public Map getJsonResponseContent(){
+    public Map getJsonResponseContent() {
         String response = this.apiResponse.content
-        if (response){
+        if (response) {
             return stringToMap(response)
         }
         return null
